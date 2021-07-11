9eaa3c70da0811b722c336a0f49aa5dd005991d3
EdoFro <35700575+EdoFro@users.noreply.github.com>
2021-07-11 19:30:15 -0400

    WIP

--- Menu-o-Matic/Menu-o-Matic.mm
+++ Menu-o-Matic/Menu-o-Matic.mm
@@ -5,7 +5,7 @@
 <hook NAME="AutomaticEdgeColor" COUNTER="13" RULE="ON_BRANCH_CREATION"/>
 <attribute_layout NAME_WIDTH="112.5 pt" VALUE_WIDTH="321.74999 pt"/>
 <attribute NAME="name" VALUE="menuOMatic"/>
-<attribute NAME="version" VALUE="v0.1.0"/>
+<attribute NAME="version" VALUE="v0.1.1"/>
 <attribute NAME="author" VALUE="EdoFro"/>
 <attribute NAME="freeplaneVersionFrom" VALUE="v1.9.0"/>
 <attribute NAME="freeplaneVersionTo" VALUE=""/>
@@ -53,7 +53,7 @@
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5 px" SHAPE_VERTICAL_MARGIN="2 px" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="2 px" MAX_WIDTH="15 cm" MIN_WIDTH="3.5 cm">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#bf5d3f" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTINCLINATION="45 pt;-9 pt;" ENDINCLINATION="57 pt;30 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#bf5d3f" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTINCLINATION="45 pt;-3 pt;" ENDINCLINATION="57 pt;30 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="Dialog" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="horizontal" COLOR="#ff0000" WIDTH="1" DASH="SOLID"/>
 <richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
@@ -146,8 +146,7 @@
       To translate the description you have to define a translation for the key 'addons.${name}.description'.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="Menu-o-Matic for Freeplane!!&#xa;Menu-o-Matic is an AddOn for Freeplane that gives the user the possibility to create his/her own dialogs with the menu items he/she prefers.&#xa;It can also include scripts." ID="ID_1642042079"/>
 </node>
 <node TEXT="changes" POSITION="left" ID="ID_1349799607">
@@ -162,10 +161,12 @@
       Change log of this add-on: append one node for each noteworthy version and put the details for each version into a child node.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="v0.1.0" ID="ID_674021294">
 <node TEXT="initial release" ID="ID_17370257"/>
+</node>
+<node TEXT="v0.1.1" ID="ID_935483166">
+<node TEXT="- new command to directly open dialog from nodes (not through customMenu node)&#xa;- added hyperlink to &quot;Launch&quot; command to &apos;customMenu&apos; pack nodes&#xa;- new command to launch all dialogs from selected node branch&#xa;- added option to return focus to map after button click&#xa;- added showLastUsedMenu command&#xa;- listed dialogs now sorted in most recently used order&#xa;- added Add-on images" ID="ID_1604082793"/>
 </node>
 </node>
 <node TEXT="license" POSITION="left" ID="ID_492801974">
@@ -186,8 +187,7 @@
       The License text has to be entered as a child of the <i>'license'</i>&nbsp;node, either as plain text or as HTML.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="MIT License&#xa;&#xa;Copyright (c) 2021 Eduardo Frohlich.&#xa;&#xa;Permission is hereby granted, free of charge, to any person obtaining a copy&#xa;of this software and associated documentation files (the &quot;Software&quot;), to deal&#xa;in the Software without restriction, including without limitation the rights&#xa;to use, copy, modify, merge, publish, distribute, sublicense, and/or sell&#xa;copies of the Software, and to permit persons to whom the Software is&#xa;furnished to do so, subject to the following conditions:&#xa;&#xa;The above copyright notice and this permission notice shall be included in all&#xa;copies or substantial portions of the Software.&#xa;&#xa;THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR&#xa;IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,&#xa;FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE&#xa;AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER&#xa;LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,&#xa;OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE&#xa;SOFTWARE." ID="ID_1379820487" VSHIFT_QUANTITY="-0.75 pt"/>
 </node>
 <node TEXT="preferences.xml" POSITION="left" ID="ID_809015286">
@@ -205,8 +205,7 @@
       <font color="#000000" face="SansSerif, sans-serif">Every property in the configuration should receive a default value in <i>default.properties</i>&nbsp;node.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="default.properties" POSITION="left" ID="ID_252406354">
 <edge COLOR="#00ffff"/>
@@ -228,8 +227,7 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="translations" POSITION="left" ID="ID_971883114">
 <edge COLOR="#7c0000"/>
@@ -254,16 +252,18 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="en" ID="ID_1296909110">
 <attribute_layout NAME_WIDTH="179.24999 pt" VALUE_WIDTH="179.24999 pt"/>
 <attribute NAME="addons.${name}" VALUE="Menu-o-Matic"/>
-<attribute NAME="addons.${name}.launchCustomDialog" VALUE="launch menu dialog"/>
+<attribute NAME="addons.${name}.launchCustomDialog" VALUE="launch menu dialog from package"/>
 <attribute NAME="addons.${name}.listCustomDialogs" VALUE="list menu dialogs"/>
 <attribute NAME="addons.${name}.packCustomMenu" VALUE="pack menu"/>
 <attribute NAME="addons.${name}.unpackCustomMenu" VALUE="unpack menu"/>
 <attribute NAME="addons.${name}.getMenuCommand" VALUE="menu item under mouse as new node"/>
+<attribute NAME="addons.${name}.lauchDialogFromNodes" VALUE="launch menu dialog directly"/>
+<attribute NAME="addons.${name}.launchDialogsFromBranch" VALUE="launch all dialog packages from branch"/>
+<attribute NAME="addons.${name}.showLastUsedMenu" VALUE="launch most recently used menu"/>
 </node>
 </node>
 <node TEXT="deinstall" POSITION="left" ID="ID_1351206087">
@@ -278,8 +278,7 @@
       List of files and/or directories to remove on uninstall
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <attribute_layout NAME_WIDTH="42.75 pt" VALUE_WIDTH="316.49999 pt"/>
 <attribute NAME="delete" VALUE="${installationbase}/addons/${name}.script.xml"/>
 <attribute NAME="delete" VALUE="${installationbase}/addons/${name}/scripts/launchCustomDialog.groovy"/>
@@ -294,6 +293,12 @@
 <attribute NAME="delete" VALUE="${installationbase}/templates/Menu-o-Matic/Menu-o-Matic template.svg"/>
 <attribute NAME="delete" VALUE="${installationbase}/doc/Menu-o-Matic/Menu-o-Matic guide.mm"/>
 <attribute NAME="delete" VALUE="${installationbase}/resources/images/Menu-o-Matic-screenshot-1.png"/>
+<attribute NAME="delete" VALUE="${installationbase}/addons/${name}/scripts/lauchDialogFromNodes.groovy"/>
+<attribute NAME="delete" VALUE="${installationbase}/addons/${name}/scripts/launchDialogsFromBranch.groovy"/>
+<attribute NAME="delete" VALUE="${installationbase}/addons/${name}/scripts/showLastUsedMenu.groovy"/>
+<attribute NAME="delete" VALUE="${installationbase}/resources/images/menuOMatic-screenshot-1.png"/>
+<attribute NAME="delete" VALUE="${installationbase}/resources/images/menuOMatic-icon.png"/>
+<attribute NAME="delete" VALUE="${installationbase}/resources/images/menuOMatic.png"/>
 </node>
 <node TEXT="scripts" POSITION="right" ID="ID_1105183990">
 <edge COLOR="#007c00"/>
@@ -412,8 +417,7 @@
       &nbsp;&nbsp;- In any case set execute_scripts_without_asking to true unless you want to annoy users.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="launchCustomDialog.groovy" ID="ID_666327274">
 <attribute_layout NAME_WIDTH="200.24999 pt" VALUE_WIDTH="200.24999 pt"/>
 <attribute NAME="menuTitleKey" VALUE="addons.${name}.launchCustomDialog"/>
@@ -474,6 +478,42 @@
 <attribute NAME="execute_scripts_without_exec_restriction" VALUE="false"/>
 <attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
 </node>
+<node TEXT="lauchDialogFromNodes.groovy" ID="ID_1980105513">
+<attribute_layout NAME_WIDTH="200.24999 pt" VALUE_WIDTH="200.24999 pt"/>
+<attribute NAME="menuTitleKey" VALUE="addons.${name}.lauchDialogFromNodes"/>
+<attribute NAME="menuLocation" VALUE="/menu_bar/edoTools/addons.${name}"/>
+<attribute NAME="executionMode" VALUE="on_single_node"/>
+<attribute NAME="keyboardShortcut" VALUE=""/>
+<attribute NAME="execute_scripts_without_asking" VALUE="true"/>
+<attribute NAME="execute_scripts_without_file_restriction" VALUE="true"/>
+<attribute NAME="execute_scripts_without_write_restriction" VALUE="true"/>
+<attribute NAME="execute_scripts_without_exec_restriction" VALUE="false"/>
+<attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
+</node>
+<node TEXT="launchDialogsFromBranch.groovy" ID="ID_108161238">
+<attribute_layout NAME_WIDTH="200.24999 pt" VALUE_WIDTH="200.24999 pt"/>
+<attribute NAME="menuTitleKey" VALUE="addons.${name}.launchDialogsFromBranch"/>
+<attribute NAME="menuLocation" VALUE="/menu_bar/edoTools/addons.${name}"/>
+<attribute NAME="executionMode" VALUE="on_single_node"/>
+<attribute NAME="keyboardShortcut" VALUE=""/>
+<attribute NAME="execute_scripts_without_asking" VALUE="true"/>
+<attribute NAME="execute_scripts_without_file_restriction" VALUE="true"/>
+<attribute NAME="execute_scripts_without_write_restriction" VALUE="true"/>
+<attribute NAME="execute_scripts_without_exec_restriction" VALUE="false"/>
+<attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
+</node>
+<node TEXT="showLastUsedMenu.groovy" ID="ID_960626023">
+<attribute_layout NAME_WIDTH="200.24999 pt" VALUE_WIDTH="200.24999 pt"/>
+<attribute NAME="menuTitleKey" VALUE="addons.${name}.showLastUsedMenu"/>
+<attribute NAME="menuLocation" VALUE="/menu_bar/edoTools/addons.${name}"/>
+<attribute NAME="executionMode" VALUE="on_single_node"/>
+<attribute NAME="keyboardShortcut" VALUE=""/>
+<attribute NAME="execute_scripts_without_asking" VALUE="true"/>
+<attribute NAME="execute_scripts_without_file_restriction" VALUE="true"/>
+<attribute NAME="execute_scripts_without_write_restriction" VALUE="false"/>
+<attribute NAME="execute_scripts_without_exec_restriction" VALUE="false"/>
+<attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
+</node>
 </node>
 <node TEXT="lib" POSITION="right" ID="ID_1186629716">
 <edge COLOR="#7c007c"/>
@@ -511,8 +551,7 @@
       &nbsp;- The files will be processed in the sequence as seen in the map.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="Menu-o-Matic.jar" ID="ID_257757258"/>
 </node>
 <node TEXT="zips" POSITION="right" ID="ID_532152967">
@@ -551,12 +590,11 @@
       &nbsp;- The files will be processed in the sequence as seen in the map.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="doc" ID="ID_108012296"/>
 <node TEXT="templates" ID="ID_1212139449"/>
 </node>
-<node TEXT="images" POSITION="right" ID="ID_817685181">
+<node TEXT="images" POSITION="right" ID="ID_817685181" VGAP_QUANTITY="2 px">
 <edge COLOR="#7c7c00"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
@@ -590,7 +628,11 @@
   </body>
 </html>
 </richcontent>
-<node TEXT="Menu-o-Matic-screenshot-1.png" ID="ID_828445446"/>
+<node TEXT="menuOMatic-screenshot-1.png" ID="ID_828445446"/>
+<node TEXT="menuOMatic-icon.png" STYLE_REF="freshNew" ID="ID_787195450"><richcontent CONTENT-TYPE="plain/markdown" TYPE="DETAILS"/>
+</node>
+<node TEXT="menuOMatic.png" STYLE_REF="freshNew" ID="ID_397986393"><richcontent CONTENT-TYPE="plain/markdown" TYPE="DETAILS"/>
+</node>
 </node>
 <node TEXT="DevTools - MoM" POSITION="right" ID="ID_989188638">
 <edge COLOR="#ff0000"/>
@@ -599,46 +641,6 @@
 </node>
 <node TEXT="Package add-on for publication" ID="ID_806418866" LINK="menuitem:_addons.devtools.releaseAddOn_on_single_node">
 <icon BUILTIN="emoji-1F5DC"/>
-</node>
-<node TEXT="DevTools" ID="ID_1227992955">
-<attribute NAME="tbActions" VALUE="[addons.devtools.checkAddOn_on_single_node, addons.devtools.releaseAddOn_on_single_node]"/>
-<attribute NAME="tbIcons" VALUE="[IconAction.emoji-1F527, IconAction.emoji-1F5DC]"/>
-<attribute NAME="tbLabels" VALUE="[Build add-on, Package add-on for publication]"/>
-<attribute NAME="tbMaxTextLength" VALUE="50" OBJECT="org.freeplane.features.format.FormattedNumber|50"/>
-<attribute NAME="tbShowIcons" VALUE="true"/>
-<attribute NAME="tbShowLabels" VALUE="false"/>
-<attribute NAME="tbTitle" VALUE="DevTools"/>
-<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      customMenu - icons only
-    </p>
-  </body>
-</html></richcontent>
-</node>
-<node TEXT="DevTools" ID="ID_1670894731">
-<attribute NAME="tbActions" VALUE="[addons.devtools.checkAddOn_on_single_node, addons.devtools.releaseAddOn_on_single_node]"/>
-<attribute NAME="tbIcons" VALUE="[IconAction.emoji-1F527, IconAction.emoji-1F5DC]"/>
-<attribute NAME="tbLabels" VALUE="[Build add-on, Package add-on for publication]"/>
-<attribute NAME="tbMaxTextLength" VALUE="50" OBJECT="org.freeplane.features.format.FormattedNumber|50"/>
-<attribute NAME="tbShowIcons" VALUE="true"/>
-<attribute NAME="tbShowLabels" VALUE="true"/>
-<attribute NAME="tbTitle" VALUE="DevTools"/>
-<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      customMenu - icons and labels
-    </p>
-  </body>
-</html></richcontent>
 </node>
 </node>
 </node>
--- resources/MoM_example.mm
+++ resources/MoM_example.mm
@@ -2,14 +2,14 @@
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <attribute_registry SHOW_ATTRIBUTES="selected"/>
 <node TEXT="Menu-o-Matic" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1090958577" MAX_WIDTH="5 cm"><hook NAME="MapStyle" background="#f9f9f8" zoom="1.1">
-    <properties save_modification_times="false" save_last_visited_node="default" mapUsesOwnSaveOptions="true" fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" save_folding="never_save_folding"/>
+    <properties mapUsesOwnSaveOptions="true" save_last_visited_node="default" save_modification_times="false" show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" save_folding="never_save_folding"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" ID="ID_118736178" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5 px" SHAPE_VERTICAL_MARGIN="2 px" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="2 px">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#bf5d3f" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTINCLINATION="45 pt;-13.5 pt;" ENDINCLINATION="57 pt;30 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#bf5d3f" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTINCLINATION="45 pt;-8.25 pt;" ENDINCLINATION="57 pt;30 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="Dialog" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="horizontal" COLOR="#2e3440" WIDTH="1" DASH="SOLID"/>
 <richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
@@ -169,7 +169,113 @@
 </node>
 <node TEXT="what if I want to reuse a dialog I recently close" POSITION="right" ID="ID_1069888192"/>
 <node TEXT="a dialog can have script too" POSITION="right" ID="ID_1992280199"/>
-<node TEXT="bye" POSITION="right" ID="ID_554468303"/>
+<node TEXT="bye" POSITION="right" ID="ID_554468303"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      import java.awt.event.*
+    </p>
+    <p>
+      //import javax.swing.event.*
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      dialogStr&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;= '_MoM_'
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      def dialogos = ui.frame.ownedWindows.findAll{it.name.startsWith(dialogStr) &amp;&amp; it.type.toString()=='NORMAL'}
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      dialogo = dialogos[0]
+    </p>
+    <p>
+      //return dialogo.windowFocusListeners
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      //removeWindowListeners(dialogo)
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      dialogo.addWindowFocusListener(
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;[windowGainedFocus : { e -&gt; c.statusInfo = 'ganado'; setLastUsed(dialogo)} ,&nbsp;
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;windowLostFocus&nbsp;&nbsp;&nbsp;: { e -&gt; c.statusInfo = dialogo.lastUsed.toString() }] as WindowFocusListener )
+    </p>
+    <p>
+      &nbsp;&nbsp;
+    </p>
+    <p>
+      def static setLastUsed(d){
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;if(d.hasProperty('lastUsed')){
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d.lastUsed = new Date()
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;} else {
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d.metaClass.lastUsed = new Date()
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;}
+    </p>
+    <p>
+      }
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      def removeWindowListeners(d){
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;d.windowFocusListeners.each{
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d.removeWindowFocusListener(it)
+    </p>
+    <p>
+      &nbsp;&nbsp;&nbsp;&nbsp;}
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html></richcontent>
+<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      .groovy
+    </p>
+  </body>
+</html></richcontent>
+</node>
 <node TEXT="hope it helps!!" POSITION="right" ID="ID_1465118130"/>
 <node TEXT="My dialog" POSITION="left" ID="ID_1653333310">
 <attribute NAME="tbActions" VALUE="[BlinkingNodeHookAction, ToggleMapOverviewAction, BoldAction, ShowFormatPanel, _script1]"/>
@@ -191,7 +297,8 @@
     </p>
   </body>
 </html></richcontent>
-<node TEXT="My dialog" ID="ID_80203147">
+<node TEXT="MY DIALOG 3" ID="ID_80203147">
+<font BOLD="false"/>
 <node TEXT="Blinking node" ID="ID_157219602" LINK="menuitem:_BlinkingNodeHookAction"/>
 <node TEXT="Map overview" ID="ID_343646915" LINK="menuitem:_ToggleMapOverviewAction">
 <icon BUILTIN="mindmap"/>
@@ -201,6 +308,36 @@
 <icon BUILTIN="emoji-1F6E0"/>
 </node>
 <node TEXT="toUpperCase.groovy" ID="ID_530821844" LINK="file:/C:/Users/Edo/Documents/GitHub/Freeplane_My-Menu-Inator/resources/toUpperCase.groovy"/>
+<node TEXT="MY DIALOG 3" ID="ID_381068800" LINK="menuitem:_addons.menuOMatic.launchCustomDialog_on_single_node">
+<attribute NAME="tbActions" VALUE="[BlinkingNodeHookAction, ToggleMapOverviewAction, BoldAction, ShowFormatPanel, _script1, _script2]"/>
+<attribute NAME="tbIcons" VALUE="[BlinkingNodeHookAction, IconAction.mindmap, BoldAction, IconAction.emoji-1F6E0, null, IconAction.info]"/>
+<attribute NAME="tbLabels" VALUE="[Blinking node, Map overview, Bold, show tool panel, toUpperCase.groovy, show node&apos;s text]"/>
+<attribute NAME="tbMaxTextLength" VALUE="50" OBJECT="org.freeplane.features.format.FormattedNumber|50"/>
+<attribute NAME="tbShowIcons" VALUE="true"/>
+<attribute NAME="tbShowLabels" VALUE="true"/>
+<attribute NAME="tbFocusToMap" VALUE="true"/>
+<attribute NAME="tbTitle" VALUE="MY DIALOG 3"/>
+<attribute NAME="_script1" VALUE="node.text = node.text.toUpperCase()"/>
+<attribute NAME="_script2" VALUE="ui.informationMessage(node.text)"/>
+<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      customMenu&nbsp;&nbsp;
+    </p>
+    <p>
+      &nbsp;&nbsp;- icons and labels
+    </p>
+    <p>
+      &nbsp;&nbsp;- return to mindmap
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true"/>
+</node>
 <node TEXT="show node&apos;s text" ID="ID_1744437958">
 <icon BUILTIN="info"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
@@ -213,8 +350,7 @@
       ui.informationMessage(node.text)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
 <html>
   <head>
@@ -225,31 +361,7 @@
       .groovy
     </p>
   </body>
-</html>
-</richcontent>
-</node>
-<node TEXT="My dialog" ID="ID_1183664647">
-<attribute NAME="tbActions" VALUE="[BlinkingNodeHookAction, ToggleMapOverviewAction, BoldAction, ShowFormatPanel, _script1, _script2]"/>
-<attribute NAME="tbIcons" VALUE="[BlinkingNodeHookAction, IconAction.mindmap, BoldAction, IconAction.emoji-1F6E0, null, IconAction.info]"/>
-<attribute NAME="tbLabels" VALUE="[Blinking node, Map overview, Bold, show tool panel, toUpperCase.groovy, show node&apos;s text]"/>
-<attribute NAME="tbMaxTextLength" VALUE="50" OBJECT="org.freeplane.features.format.FormattedNumber|50"/>
-<attribute NAME="tbShowIcons" VALUE="true"/>
-<attribute NAME="tbShowLabels" VALUE="true"/>
-<attribute NAME="tbTitle" VALUE="My dialog"/>
-<attribute NAME="_script1" VALUE="node.text = node.text.toUpperCase()"/>
-<attribute NAME="_script2" VALUE="ui.informationMessage(node.text)"/>
-<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      customMenu - icons and labels
-    </p>
-  </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
