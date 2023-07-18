f8b591348c9049f8669eb42357d3be8061ab3b0c
EdoFro <35700575+EdoFro@users.noreply.github.com>
2023-07-18 11:06:08 -0400

    Update LaunchDialog.groovy
    Refactoring showDialog

--- Menu-o-Matic/src/main/groovy/LaunchDialog.groovy
+++ Menu-o-Matic/src/main/groovy/LaunchDialog.groovy
@@ -10,7 +10,6 @@ import groovy.time.TimeDuration
 
 
 import edofro.menuomatic.PackMenu                   as PM
-import edofro.menuomatic.WSE_redux                  as WSE
 import edofro.menuomatic.DialogKeyboardNavigation   as DKBN
     
 import org.freeplane.core.ui.components.UITools     as ui
@@ -54,40 +53,7 @@ class LaunchDialog{
         def titulo = PM.respuestaDialogo(titulos,msg,title)
         dialogos.find{it.title == titulo}?.show()
     }
-    
-    def static showDialogFromMD(MD){
-        md = MD
-        def dialogName = dialogStr + md.title.replace(' ' ,'_')
-        prefDimension = new Dimension(( md.showLabels?100:0) + (md.showIcons?30:0 ) ,md.showIcons?30:25)
-        minDimension = new Dimension(30 ,md.showIcons?30:25)
-        
-        def dialogo = ui.frame.ownedWindows.find{it.name == dialogName && it.type.toString()=='NORMAL'}
-        if( !dialogo ) {
-            c.statusInfo = '------------ new menu dialog created --------------'
-            dialogo = swingBuilder.dialog(
-                title              : md.title,
-                name               : dialogName,
-                modal              : false,
-                locationRelativeTo : ui.frame,
-                minimumSize        : new Dimension(30,70),
-                owner              : ui.frame
-                //defaultCloseOperation: JFrame.DISPOSE_ON_CLOSE,
-            ) {}
-        } else {
-            c.statusInfo = '------------- existing menu dialog reused -------------------'
-            dialogo.getContentPane().removeAll()
-        }        
-        def cols = md.showLabels?1:buttonCols(md.actions.size(),maxButtonsHoriz)
-        dialogo.add(creaContenido(cols)) //TODO: 
-        dialogo.pack()
-        dialogo.show()
-        setLastUsed(dialogo)
-        setWindowFocusListeners(dialogo)
 
-        DKBN.addArrowMoves(dialogo)
-        DKBN.addEscapeAction(dialogo)
-    }
-    
     // endregion
 
     // region dialog methods
@@ -108,32 +74,106 @@ class LaunchDialog{
                 [windowGainedFocus : { e -> setLastUsed(d)} ,
                  windowLostFocus   : { e -> }] as WindowFocusListener )
     }
-    
 
     // endregion
 
+    // region node information
+
+    def static isCustomMenuPack(n){
+        n.attributes.containsKey(tb.actions)
+    }
+
+    // endregion
+
+
     def static show(nodo){
         if(isCustomMenuPack(nodo)){
-            showDialogFromMD(new PM.MenuData(nodo))
+            md = new PM.MenuData(nodo)
+            prefDimension = new Dimension(( md.showLabels?100:0) + (md.showIcons?30:0 ) ,md.showIcons?30:25)
+            minDimension = new Dimension(30 ,md.showIcons?30:25)
+            def panelName = md.title.replace(' ' ,'_')
+            showDialog(panelName)
         } else {
             c.statusInfo = 'selected node is not a customMenu node'
         }
 
     }
     
+    //region creating/showing dialog menu
+
+    def static showDialogFromMD(MD){
+        md = MD
+        prefDimension = new Dimension(( md.showLabels?100:0) + (md.showIcons?30:0 ) ,md.showIcons?30:25)
+        minDimension = new Dimension(30 ,md.showIcons?30:25)
+        def panelName = md.title.replace(' ' ,'_')
+        showDialog(panelName)
+    }
+
+    def static showDialog(panelName){
+        def dialogo = getEmptyDialog(dialogStr + panelName)
+        dialogo.add(panelContenido(panelName))
+        dialogo.pack()
+        dialogo.setVisible(true)
+        setLastUsed(dialogo)
+        setWindowFocusListeners(dialogo)
+
+        DKBN.addArrowMoves(dialogo)
+        DKBN.addEscapeAction(dialogo)
+    }
+
+    def static getEmptyDialog(String dialogName){
+        def dialogo = ui.frame.ownedWindows.find{it.name == dialogName && it.type.toString()=='NORMAL'}
+        if( !dialogo ) {
+            c.statusInfo = '------------ new menu dialog created --------------'
+            dialogo = swingBuilder.dialog(
+                    title              : md.title,
+                    name               : dialogName,
+                    modal              : false,
+                    locationRelativeTo : ui.frame,
+                    minimumSize        : new Dimension(30,70),
+                    owner              : ui.frame
+                    //defaultCloseOperation: JFrame.DISPOSE_ON_CLOSE,
+            ) {}
+        } else {
+            c.statusInfo = '------------- existing menu dialog reused -------------------'
+            dialogo.getContentPane().removeAll()
+        }
+        return dialogo
+    }
+
+    // endregion
+
+    //region creating panel
+
+    def static getCols(){
+        md.showLabels?1:buttonCols(md.actions.size(),maxButtonsHoriz)
+    }
+
     def static buttonCols(x,cMax){
         int L = (cMax+x-1)/cMax
         int c = (x+L-1)/L
     }
 
-    def static isCustomMenuPack(n){
-        n.attributes.containsKey(tb.actions)
+    def static panelContenido(){
+        def cols = getCols()
+        def panelName = 'miPanel'
+        return panelContenido(cols, panelName)
     }
 
-    def static creaContenido(cols){
+    def static panelContenido(Integer cols){
+        def panelName = 'miPanel'
+        return panelContenido(cols, panelName)
+    }
+
+    def static panelContenido(String panelName){
+        def cols = getCols()
+        return panelContenido(cols, panelName)
+    }
+
+    def static panelContenido(Integer cols, String panelName){
         return swingBuilder.panel(
-                name   : 'miPanel'
                 layout : new GridLayout(0, cols),
+                name   : panelName
             ){
     //           toolBar(
     //           layout: new FlowLayout(FlowLayout.LEFT, 0, 0)
