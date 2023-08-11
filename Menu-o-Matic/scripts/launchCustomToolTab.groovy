e02f071061f5af4b3c5d263ec816e9f9248e7ed5
EdoFro <35700575+EdoFro@users.noreply.github.com>
2023-08-11 01:13:50 -0400

    Update launchCustomToolTab.groovy

--- Menu-o-Matic/scripts/launchCustomToolTab.groovy
+++ Menu-o-Matic/scripts/launchCustomToolTab.groovy
@@ -2,10 +2,10 @@ import edofro.menuomatic.LaunchTabPane as LTP
 import edofro.menuomatic.LaunchDialog as LD
 
 
-//println "tab eliminada: ${LTP.removeTab('MoM')}"
+//println "tab eliminada: ${LTP.removeTab('MDI3')}"; return
+
 
 
 boolean openInTabPane = true
 
 LD.show(node, openInTabPane)
-
