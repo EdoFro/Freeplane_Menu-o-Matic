af7e751bb880b07dcd99360003ce7a252f844b0d
EdoFro <35700575+EdoFro@users.noreply.github.com>
2023-08-30 18:07:54 -0400

    new MenuOMatic toolbar (with separators)

--- Menu-o-Matic/scripts/insertMenuOMaticPackage.groovy
+++ Menu-o-Matic/scripts/insertMenuOMaticPackage.groovy
@@ -1,16 +1,17 @@
 import edofro.menuomatic.PackMenu            as PM
 
+
 Map MoMmenuData = [
-        'actions'       : ['addons.menuOMatic.launchCustomDialog_on_single_node', 'addons.menuOMatic.changePackParameters_on_single_node', 'addons.menuOMatic.packCustomMenu_on_single_node', 'addons.menuOMatic.unpackCustomMenu_on_single_node', 'addons.menuOMatic.lauchDialogFromNodes_on_single_node', 'addons.menuOMatic.launchDialogsFromBranch_on_single_node', 'addons.menuOMatic.listCustomMenusFromMap_on_single_node'],
-        'icons'         : ['IconAction.emoji-1F680', 'IconAction.emoji-1F527', 'IconAction.emoji-1F4E6', 'IconAction.emoji-1F468-200D-1F527', 'IconAction.emoji-2728', 'IconAction.emoji-1F387', 'IconAction.mindmap'],
-        'labels'        : ['launch menu dialog', 'Change pack parameters', 'pack menu', 'unpack menu', 'launch directly', 'launch all', 'list menu packages from map'],
-        'maxTextLength' : 50,
-        'showIcons'     : true,
-        'showLabels'    : true,
-        'title'         : 'Menu-o-Matic',
-        'scripts'       : [],
-        'tabName'       : 'MoM',
-        'autoLaunch'    : true
+        'actions':['addons.menuOMatic.lauchDialogFromNodes_on_single_node', 'addons.menuOMatic.launchCustomDialog_on_single_node', '_separator',                'addons.menuOMatic.launchDialogsFromBranch_on_single_node', 'addons.menuOMatic.listCustomMenusFromMap_on_single_node', '_separator', 'addons.menuOMatic.packCustomMenu_on_single_node', 'addons.menuOMatic.changePackParameters_on_single_node', 'addons.menuOMatic.unpackCustomMenu_on_single_node'],
+        'icons':["IconAction.emoji-2728", "IconAction.emoji-1F680", null, "IconAction.emoji-1F387", "IconAction.mindmap", null, "IconAction.emoji-1F4E6", "IconAction.emoji-1F527", "IconAction.emoji-1F468-200D-1F527"],
+        'labels':['launch directly', 'launch menu dialog', '----', 'launch all', 'list menu packages from map', '----', 'pack menu', 'Change pack parameters', 'unpack menu'],
+        'maxTextLength':50,
+        'showIcons':true,
+        'showLabels':true,
+        'title':'Menu-o-Matic',
+        'scripts':[],
+        'tabName':'MoM',
+        'autoLaunch':false
 ]
 
 
